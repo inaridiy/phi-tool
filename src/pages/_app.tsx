efa230284eae67696165182f0f961d66b42c1295
inaridiy <inainainariinainari@gmail.com>
2022-11-22 18:11:41 +0900

    feat: rainbow kit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,38 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { NoSSR } from "../components";
 
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+if (!process.env.ALCHEMY_ID) throw new Error("ALCHEMY_ID");
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Phi Tools",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <NoSSR>
-      <Component {...pageProps} />
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains}>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
     </NoSSR>
   );
 }
